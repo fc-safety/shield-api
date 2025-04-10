922b30ee06ea62b6538b07d095c70b645bdc245f
Jared Gibson <jared.gibson@live.com>
2025-04-10 12:23:20 -0600

    Fix site update schema

--- src/clients/sites/dto/update-site.dto.ts
+++ src/clients/sites/dto/update-site.dto.ts
@@ -1,4 +1,19 @@
+import { Prisma } from '@prisma/client';
 import { createZodDto } from 'nestjs-zod';
+import { createAddressSchema } from 'src/common/schema';
+import { z } from 'zod';
 import { CreateSiteSchema } from './create-site.dto';
 
-export class UpdateSiteDto extends createZodDto(CreateSiteSchema.partial()) {}
+const UpdateSiteSchema = CreateSiteSchema.partial()
+  .omit({ address: true, subsites: true })
+  .extend({
+    address: z.object({
+      update: createAddressSchema.partial(),
+    }),
+    subsites: z.object({
+      set: z.array(z.object({ id: z.string() })),
+    }),
+  })
+  .partial() satisfies z.Schema<Prisma.SiteUpdateInput>;
+
+export class UpdateSiteDto extends createZodDto(UpdateSiteSchema) {}
