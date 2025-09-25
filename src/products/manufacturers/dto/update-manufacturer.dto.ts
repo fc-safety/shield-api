89074f1a93b96fc90e766b4ca6c3ba610832fbf0
Jared Gibson <jared.gibson@live.com>
2025-09-25 17:25:07 -0600

    Allow disconnectable client on product, manufacturers, and categories

--- src/products/manufacturers/dto/update-manufacturer.dto.ts
+++ src/products/manufacturers/dto/update-manufacturer.dto.ts
@@ -1,6 +1,17 @@
 import { createZodDto } from 'nestjs-zod';
+import { Prisma } from 'src/generated/prisma/client';
+import z from 'zod';
 import { CreateManufacturerSchema } from './create-manufacturer.dto';
 
 export class UpdateManufacturerDto extends createZodDto(
-  CreateManufacturerSchema.partial(),
+  CreateManufacturerSchema.extend({
+    client: z
+      .object({
+        connect: z.object({
+          id: z.string(),
+        }),
+        disconnect: z.boolean(),
+      })
+      .partial(),
+  }).partial() satisfies z.Schema<Prisma.ManufacturerUpdateInput>,
 ) {}
--- src/products/products/dto/update-product.dto.ts
+++ src/products/products/dto/update-product.dto.ts
@@ -1,6 +1,17 @@
 import { createZodDto } from 'nestjs-zod';
+import { Prisma } from 'src/generated/prisma/client';
+import { z } from 'zod';
 import { CreateProductSchema } from './create-product.dto';
 
 export class UpdateProductDto extends createZodDto(
-  CreateProductSchema.partial(),
+  CreateProductSchema.extend({
+    client: z
+      .object({
+        connect: z.object({
+          id: z.string(),
+        }),
+        disconnect: z.boolean(),
+      })
+      .partial(),
+  }).partial() satisfies z.Schema<Prisma.ProductUpdateInput>,
 ) {}
