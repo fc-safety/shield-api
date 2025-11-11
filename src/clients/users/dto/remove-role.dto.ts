645ded4147c2accc1eb4e016e788a35d9238f34a
Jared Gibson <jared.gibson@live.com>
2025-11-11 15:45:41 -0700

    Rm unused file

--- src/clients/users/dto/remove-role.dto.ts
+++
@@ -1,7 +0,0 @@
-import { createZodDto } from 'nestjs-zod';
-import { z } from 'zod';
-
-const RemoveRoleSchema = z.object({
-  roleId: z.string(),
-});
-export class RemoveRoleDto extends createZodDto(RemoveRoleSchema) {}
