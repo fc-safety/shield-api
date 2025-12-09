31f33e8e5d2a04da878e91ef558842d047b175b1
Jared Gibson <jared.gibson@live.com>
2025-12-09 11:00:09 -0700

    Add Sentry

---
+++ src/instrument.ts
@@ -0,0 +1,8 @@
+import * as Sentry from '@sentry/nestjs';
+
+Sentry.init({
+  dsn: 'https://eb6b74337b6655d78417b881cd519d9c@o4510505981116416.ingest.us.sentry.io/4510506176479232',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
